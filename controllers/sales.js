47d240ebada4af294d4e478a7dfc35ef57059a67
antoniomgatto <antonio.gatto@gobee.com.br>
2019-03-08 23:22:59 -0300

    removendo rotas desnecessárias para sales e alterado create sale para obter usuário da sessão

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,30 +1,10 @@
-const Sequelize = require('sequelize');
 const { Sale, SaleItem } = require('../models');
 
 module.exports = {
-  index(req, res) {
-    const limit = (req.query.limit || 20);
-    const offset = (req.query.page > 1 ? (req.query.page - 1) : 0 || 0) * limit;
-
-    return Sale
-      .findAll({
-        order: [
-          ['id', 'ASC'],
-        ],
-        include: [
-          {
-            model: SaleItem,
-            as: 'items',
-          },
-        ],
-        offset,
-        limit,
-      })
-      .then(sales => res.json(sales));
-  },
   create(req, res) {
+    const body = { ...req.body, ...{ companyId: req.user.id } };
     return Sale
-      .create(req.body, {
+      .create(body, {
         include: [
           {
             model: SaleItem,
@@ -33,55 +13,6 @@ module.exports = {
         ],
       })
       .then(sale => res.status(201).json(sale))
-      .catch(exception => res.status(500).send(exception));
-  },
-  show(req, res) {
-    const { id } = req.params;
-    return Sale
-      .findById(id, {
-        include: [
-          {
-            model: SaleItem,
-            as: 'items',
-          },
-        ],
-      })
-      .then((sale) => {
-        if (sale == null) return res.status(404).end();
-        return res.json(sale);
-      })
-      .catch(exception => res.status(500).send(exception));
-  },
-  update(req, res) {
-    const { id } = req.params;
-    return Sale
-      .findById(id, {
-        include: [
-          {
-            model: SaleItem,
-            as: 'items',
-          },
-        ],
-      })
-      .then((sale) => {
-        if (sale == null) return res.status(404).end();
-        return sale.update(req.body);
-      })
-      .then((sale) => {
-        if (sale instanceof Sequelize.ValidationError) return res.status(422).send(sale);
-        return res.json(sale);
-      })
-      .catch(exception => res.status(500).send(exception));
-  },
-  delete(req, res) {
-    const { id } = req.params;
-    return Sale
-      .findById(id)
-      .then((sale) => {
-        if (sale == null) return res.status(404).end();
-        return sale.destroy();
-      })
-      .then(() => res.status(204).send())
       .catch(exception => res.status(500).send(exception));
   },
 };
--- routes/sales.js
+++ routes/sales.js
@@ -3,10 +3,6 @@ const express = require('express');
 const router = express.Router();
 const salesController = require('../controllers/sales');
 
-router.get('/', salesController.index);
 router.post('/', salesController.create);
-router.get('/:id', salesController.show);
-router.put('/:id', salesController.update);
-router.delete('/:id', salesController.delete);
 
 module.exports = router;
