f3555f9518fb95bfa7246b19ae78196c56fffebd
antoniomgatto <antonio.gatto@gobee.com.br>
2019-03-25 15:02:47 -0300

    enviando dados do usuário para o firebase

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,11 +1,21 @@
 const { Customer } = require('../models');
+const moment = require("moment");
 const firebase = require('firebase');
 
 module.exports = {
   create(req, res) {
     return Customer.create(req.body)
       .then((customer) => {
-
+        firebase
+          .database()
+          .ref(`users/${customer.uid}/info`)
+          .set({
+            fullName: customer.fullName,
+            email: customer.email,
+            document: customer.document,
+            phoneNumber: customer.phoneNumber,
+            birthday: moment(customer.birthday).format('YYYY-MM-DD'),
+          });
         res.status(201).json(customer);
       })
       .catch(exception => res.status(500).send(exception));
