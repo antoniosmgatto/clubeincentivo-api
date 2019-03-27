7dbd98ce1e3b74af0c51f4d006221d604f28c212
antoniomgatto <antonio.gatto@gobee.com.br>
2019-03-27 11:15:29 -0300

    associando vendas recentes ao customer que acabou de ser cadastrado

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,6 +1,10 @@
-const { Customer } = require('../models');
-const moment = require("moment");
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+const moment = require('moment');
+const { Op } = require('sequelize');
 const firebase = require('firebase');
+const salesFirebase = require('../queues/sales-firebase');
+const { Customer, Sale } = require('../models');
 
 module.exports = {
   create(req, res) {
@@ -16,6 +20,24 @@ module.exports = {
             phoneNumber: customer.phoneNumber,
             birthday: moment(customer.birthday).format('YYYY-MM-DD'),
           });
+
+        // migrating sales to customer
+        Sale.findAll({
+          where: {
+            purchaseDate: {
+              [Op.lt]: moment().hours(-1),
+            },
+            clientDocument: customer.document,
+            customerId: null,
+            onBalance: false,
+          },
+        }).then((sales) => {
+          sales.forEach((sale) => {
+            sale
+              .update({ customerId: customer.id })
+              .then(saleSave => salesFirebase.createJob({ saleId: saleSave.id }));
+          });
+        });
         res.status(201).json(customer);
       })
       .catch(exception => res.status(500).send(exception));
