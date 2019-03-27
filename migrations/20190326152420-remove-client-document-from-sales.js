97b7f9a0f2f68b73b27cbce7dbc3d9acbf3e5da1
antoniomgatto <antonio.gatto@gobee.com.br>
2019-03-26 22:18:36 -0300

    desfeito remoção do campo client document de sales

--- migrations/20190326152420-remove-client-document-from-sales.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = {
-  // eslint-disable-next-line no-unused-vars
-  up: (queryInterface, _Sequelize) => queryInterface.removeColumn('sales', 'client_document'),
-
-  // eslint-disable-next-line no-unused-vars
-  down: (_queryInterface, _Sequelize) => {},
-};
